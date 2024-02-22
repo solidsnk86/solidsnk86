9cae407ce1b2fa3b9fc3eff32f51311542da5615
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-02-22 02:35:16 -0300

    1up

--- src/index.js
+++ src/index.js
@@ -1,40 +1,50 @@
-import { promises as fs } from 'fs'
-import fetch from 'node-fetch'
+import { promises as fs } from "fs";
+import fetch from "node-fetch";
 
-import { PLACEHOLDERS, NUMBER_OF } from './constants.js'
+import { PLACEHOLDERS, NUMBER_OF } from "./constants.js";
 
-const YOUTUBE_NEOTECS_CHANNEL_ID = 'PLTmuoTGunlv7T7pyHEzlG2PaqniaVCprL'
+const YOUTUBE_NEOTECS_CHANNEL_ID = "PLTmuoTGunlv7T7pyHEzlG2PaqniaVCprL";
 
-
-const getLatestYoutubeVideos = ({ channelId } = { channelId: YOUTUBE_NEOTECS_CHANNEL_ID }) =>
+const getLatestYoutubeVideos = (
+  { channelId } = { channelId: YOUTUBE_NEOTECS_CHANNEL_ID }
+) =>
   fetch(
     `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${channelId}&maxResults=${NUMBER_OF.VIDEOS}&key=${process.env.YOUTUBE_API_KEY}`
   )
     .then((res) => res.json())
-    .then((videos) => videos.items)
+    .then((videos) => videos.items);
 
-const generateYoutubeHTML = ({ title, videoId }) => `
-<a href='https://youtu.be/${videoId}' target='_blank'>
-  <img width='30%' src='https://i.ytimg.com/vi/${videoId}/sddefault.jpg' alt='${title}' />
-</a>`;
-
-(async () => {
-
-  const [template, videos] = await Promise.all([
-    fs.readFile('./src/README.md.tpl', { encoding: 'utf-8' }),
-    getLatestYoutubeVideos(),
-  ])
-
-  const latestYoutubeVideos = (videos || [])
-    .map(({ snippet }) => {
-      const { title, resourceId } = snippet
-      const { videoId } = resourceId
-      return generateYoutubeHTML({ videoId, title })
-    })
-    .join('')
-
-  const newMarkdown = template
-    .replace(PLACEHOLDERS.LATEST_YOUTUBE, latestYoutubeVideos)
-
-  await fs.writeFile('README.md', newMarkdown)
-})()
+    const generateYoutubeHTML = ({ title, videoId }) => `
+    <a href='https://youtu.be/${videoId}' target='_blank'>
+      <img width='30%' src='https://i.ytimg.com/vi/${videoId}/sddefault.jpg' alt='${title}' />
+    </a>`;
+    
+    (async () => {
+      try {
+        const [template, videos] = await Promise.all([
+          fs.readFile("./src/README.md.tpl", { encoding: "utf-8" }),
+          getLatestYoutubeVideos(),
+        ]);
+    
+        if (!videos) {
+          throw new Error("Unable to fetch YouTube videos.");
+        }
+    
+        const latestYoutubeVideos = videos
+          .map(({ snippet }) => {
+            const { title, resourceId } = snippet;
+            const { videoId } = resourceId;
+            return generateYoutubeHTML({ videoId, title });
+          })
+          .join("");
+    
+        const newMarkdown = template.replace(
+          PLACEHOLDERS.LATEST_YOUTUBE,
+          latestYoutubeVideos
+        );
+    
+        await fs.writeFile("README.md", newMarkdown);
+      } catch (error) {
+        console.error("Error:", error.message);
+      }
+    })();
